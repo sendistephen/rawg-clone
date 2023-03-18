a547c3ac5abb6fe7a37c3827ec2cfa46f9a4e1d3
sendistephen <sendi.steve2@gmail.com>
2023-03-18 22:17:04 +0300

    Add comments to the getCroppedImageUrl function

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,13 +1,14 @@
 /**
- * 
- * @param url 
- * @returns 
+ * Function that returns the URL of an image cropped to a specific size.
+ * @param url - The original URL of the image.
+ * @returns The URL of the cropped image.
  */
 const getCroppedImageUrl = (url: string) => {
+	// Find the index of the "media/" substring in the URL.
 	const target = 'media/';
+	// Find the index of the starting point of the media link and add the target length
 	const index = url.indexOf(target) + target.length;
-	console.log(url.slice(0, index));
-	console.log(url.slice(index));
+	// Return the cropped URL by adding "crop" followed by specified dimensions and then the remaining part from original URL
 	return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
 };
 export default getCroppedImageUrl;
