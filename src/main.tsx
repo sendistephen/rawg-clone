c2fcb4a1ef484b4086aa24a429fec67f7c824e6b
sendistephen <sendi.steve2@gmail.com>
2023-04-26 13:46:28 +0300

    Integrate react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './index.css';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import theme from './theme';
@@ -7,12 +9,17 @@ import theme from './theme';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		{/* wrap the App in ChakraProvider */}
-		<ChakraProvider theme={theme}>
-			<ColorModeScript initialColorMode={theme.config.initialColorMode} />
-			<RouterProvider router={router} />
-		</ChakraProvider>
+		<QueryClientProvider client={queryClient}>
+			<ChakraProvider theme={theme}>
+				<ColorModeScript initialColorMode={theme.config.initialColorMode} />
+				<RouterProvider router={router} />
+				<ReactQueryDevtools />
+			</ChakraProvider>
+		</QueryClientProvider>
 	</React.StrictMode>
 );
